874249b6a043f4006eccfdd27f09a4f7b6d1bae9
unknown <olafinosrcc@gmail.com>
2022-09-04 17:38:30 +0200

    Added templates with bootstrap

--- polecacz/views.py
+++ polecacz/views.py
@@ -7,7 +7,7 @@ from polecacz.models import Game, Opinion
 
 class IndexView(generic.ListView):
     template_name = 'polecacz/index.html'
-    context_object_name = 'game'
+    context_object_name = 'top_games'
 
     def get_queryset(self):
         return Game.objects.order_by('rank')[:10]
@@ -22,9 +22,28 @@ class GameListView(generic.ListView):
     model = Game
     template_name = 'polecacz/game_list.html'
     context_object_name = 'games_list'
+    paginate_by = 20
+    ordering = ['rank']
+    order_mapping = {
+        "rank": "Ranking rosnąco",
+        "-rank": "Ranking malejąco",
+        "name": "Alfabetycznie A-Z",
+        "-name": "Alfabetycznie Z-A",
+        "rating": "Średnia rosnąco",
+        "-rating": "Średnia malejąco",
+    }
 
-    def get_queryset(self):
-        return Game.objects.order_by('rank')[:10]
+    def get_ordering(self):
+        ordering = self.request.GET.get('ordering', 'rank')
+        return ordering
+
+    def get_context_data(self, **kwargs):
+        context = super(GameListView, self).get_context_data(**kwargs)
+        ordering = self.request.GET.get('ordering', 'rank')
+        order_name = self.order_mapping[ordering]
+        context['ordering'] = ordering
+        context['order_name'] = order_name
+        return context
 
 
 class OpinionDetail(generic.DetailView):
