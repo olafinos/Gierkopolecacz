5ef54e709f34a7a64eb3ed5cb285a9c06930686e
unknown <olafinosrcc@gmail.com>
2022-09-20 18:45:20 +0200

    Debug turned on

--- gierkopolecacz/settings.py
+++ gierkopolecacz/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-28j!9aq*r(-ll^*k8uqv-)ev#g$#b^1w#()okq^u9knll)0%u2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'gierkopolecacz.herokuapp.com']
 
